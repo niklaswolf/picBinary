5782aeb09b3d7ff6e8ac3de19cdc73b5bc751ada
Niklas Wolf <wolfniklas94@web.de>
2016-06-25 21:31:53 +0200

    version 2 with live-preview and automatic snapshot

--- medienkunst/calculate.php
+++ medienkunst/calculate.php
@@ -1,55 +1,37 @@
 <?php
-$file = $_FILES['picture'];
+const rasterSize = 20;
+
+$file = $_FILES['webcam'];
 $a = move_uploaded_file($file['tmp_name'], 'pictures/source.jpg');
 //echo "Picture uploaded!";
-
 $imageSize = getimagesize('pictures/source.jpg');
 $width = $imageSize[0];
 $height = $imageSize[1];
+$widthRest = $width%rasterSize;
+$heightRest = $height%rasterSize;
 
 $img = imagecreatefromjpeg('pictures/source.jpg');
+$img = imagecrop($img, ['x' => 0, 'y' => 0, 'width' =>  $width-$widthRest, 'height' => $height-$heightRest]);
+imagefilter($img, IMG_FILTER_PIXELATE, rasterSize);
+imagefilter($img, IMG_FILTER_GRAYSCALE);
 
-$rasterX = 60;
-$rasterY = 60;
-$xInterval = round($width/$rasterX);
-$yInterval = round($height/$rasterY);
-
-$h=0;
-for ($w=0; $w<$rasterX; $w++){
-	$colorArray = array();
-	$x = $w*$xInterval;
-	$y = $h*$yInterval;
-	$xStop = $x + $xInterval;
-	$yStop = $y + $yInterval;
-	
-	$avgRed = 0;
-	$avgGreen = 0;
-	$avgBlue = 0;
-	$counter = 0;
-	for($x; $x< $xStop; $x++){
-		for ($y; $y< $yStop; $y++){
-			$colors = imagecolorsforindex($img, imagecolorat($img, $x, $y));
-			$avgRed += $colors['red'];
-			$avgGreen += $colors['green'];
-			$avgBlue += $colors['blue'];
-			$counter++;
+for ($w=0; $w<$width-$widthRest; $w+=rasterSize){
+	for ($h=0; $h<$height-$heightRest; $h+=rasterSize){
+		$colors = imagecolorsforindex($img, imagecolorat($img, $w, $h));
+		$grayValue = $colors['red'];
+		if ($grayValue <= 127){
+			// paint black
+			imagefilledrectangle($img, $w, $h, $w+rasterSize-1, $h+rasterSize-1, imagecolorresolve($img, 0, 0, 0));
 		}
-	}
-	$avgRed = round($avgRed/$counter);
-	$avgGreen = round($avgGreen/$counter);
-	$avgBlue = round($avgBlue/$counter);
-	echo "<span style='color: rgb(".$avgRed.",".$avgGreen.",".$avgBlue."); width: ".$xInterval."px'>bla</span>";
-	
-	
-	//go to the next row
-	if($w==$rasterX-1){
-		$h++;
-		$w = 0;
-		echo '<br>';
-	}
-	if($h==$rasterY-1){
-		break;
+		else {
+			//paint white
+			imagefilledrectangle($img, $w, $h, $w+rasterSize-1, $h+rasterSize-1, imagecolorresolve($img, 255, 255, 255));
+		}
 	}
 }
 
+imagejpeg($img, 'pictures/modified.jpg');
+
 ?>
+<img src="pictures/modified.jpg?time=<?=time()?>">
+
--- medienkunst/index.php
+++ medienkunst/index.php
@@ -1,13 +1,73 @@
 <?php
 ?>
 <html>
+
+<style>
+	#picture-wrapper {
+		display: flex;
+	}
+	#live {
+		cursor: pointer;
+	}
+	#live, #result {
+		flex: 0 0 50%;
+		position: relative;
+	}
+	#preview {
+		/margin: auto;
+	}
+	#face-guide {
+		position: absolute;
+		width: 460px;
+		height: 620px;
+		margin: 50px 0 0 130px;
+		top: 0;
+		left: 0;
+		z-index: 2;
+		border: 4px solid;
+		border-radius: 40%;
+		text-align: center;
+		line-height: 620px;
+		font-size: 10em;
+		font-family: sans-serif;
+		color: rgba(255,255,255,0.5);
+	}
+</style>
 <body>
-
-<form action="calculate.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="picture" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
-</form>
-
+	<div id="picture-wrapper">
+		<div id="live">
+			<div id="preview"></div>
+			<div id="face-guide">Klick!</div>
+		</div>
+		<div id="result"></div>
+	</div>
 </body>
+
+<script src="webcamjs/webcam.js"></script>
+<script type="text/javascript">
+
+	Webcam.set({width: 1280, height: 720, dest_width: 1280, dest_height: 720, crop_width: 720, crop_height: 720});
+	Webcam.attach("#preview");
+	
+    function countdown (count){
+		if(count==0){
+			document.getElementById('face-guide').innerHTML = 'Klick!';
+			Webcam.snap( function(data_uri) {
+				Webcam.upload(data_uri, 'calculate.php', function (code, text){
+					document.getElementById('result').innerHTML = text;
+				});  
+	        });
+		}
+		else {
+			document.getElementById('face-guide').innerHTML = count;
+			setTimeout(function(){
+				countdown(count-1);
+			}, 1000);
+		}
+    }
+
+    document.getElementById('live').addEventListener('click', function(){
+		countdown(3);
+    });
+</script>
 </html>
